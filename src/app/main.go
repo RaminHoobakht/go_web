580346f8ae964071e1d83de32ebcdcf3420f978f
hinata <hinatachikao1971@gmail.com>
2025-01-30 17:02:06 +0330

    before creating new source file

--- src/app/main.go
+++ src/app/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
 func main() {
 
-	/* create an end point */
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my site ...")
-	})
-
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	fmt.Println("Server bootstrapping is started ...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("\n #(14:19:35): The End ...")
+    fmt.Println("\n #(17:02:06): The End ...")
 }
