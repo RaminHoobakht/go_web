22c0ecb739b54b75edc97f4d175c90fd3fe35564
hinata <hinatachikao1971@gmail.com>
2025-01-30 18:49:43 +0330

    new source file

--- src/app/main.go
+++ src/app/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-    fmt.Println("\n #(17:02:06): The End ...")
+	my_router := mux.NewRouter()
+
+	my_router.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "the name of the book is: %s on page: %s", title, page)
+	})
+
+	fmt.Println("server bootstrapping is running ...")
+	http.ListenAndServe(":8080", my_router)
+
+	fmt.Println("\n #(17:02:06): The End ...")
 }
