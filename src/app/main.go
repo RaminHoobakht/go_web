ccf03142081f68f2d1d44031cc2683efb131d6a9
hinata <hinatachikao1971@gmail.com>
2025-01-30 08:52:30 +0330

    before creating new source file

--- src/app/main.go
+++ src/app/main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
+    "fmt"
 )
 
 func main() {
-	
-	http.HandleFunc("/hello", func(resp http.ResponseWriter, req *http.Request){
-		fmt.Fprintf(resp,"Hello Hinata, How is learning Go programming?")
-	})
 
-	fmt.Println("Start Hinata Server ...")
-	http.ListenAndServe(":8080", nil)
-	
-	fmt.Println("\n The End ...")
+    fmt.Println("\n #(08:52:30): The End ...")
 }
