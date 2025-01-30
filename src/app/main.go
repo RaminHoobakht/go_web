590784a5e374eb58c98c2ae3ff33dd4d0797b48e
hinata <hinatachikao1971@gmail.com>
2025-01-30 14:19:35 +0330

    before creating new source file

--- src/app/main.go
+++ src/app/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
 
-    fmt.Println("\n #(14:10:27): The End ...")
+	/* create an endpoint to say hello */
+	http.HandleFunc("/hello", say_hello)
+
+	/* create an endpoint to say good bye */
+	http.HandleFunc("/bye", say_goodbye)
+
+	/* message to inform for starting server  */
+	fmt.Println("http://localhost:8080 server is starting ...")
+
+	/* listen to the incoming request from http://localhost:8080 */
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("\n #(14:10:27): The End ...")
+}
+
+/* -------------------------------------------------------------------------------- */
+
+func say_hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Hinata, How is learning Go programming ...\n")
+}
+
+func say_goodbye(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Good Bye, Take care ...\n")
 }
